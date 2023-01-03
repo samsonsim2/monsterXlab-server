804db346ab6d3a520a5771252f30f3578e99342f
YOUR_GITHUB_USERNAME <106437135+samsonsim2@users.noreply.github.com>
2023-01-03 23:36:30 +0800

    added more preflight  cors

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ cloudinary.config({
 app.use(cors())
 app.use(express.json())
 app.use(fileUpload({ useTempFiles: true }))
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  next()
+})
 app.use((req, res, next) => {
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
